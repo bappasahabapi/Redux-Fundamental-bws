9818cb3280a0b7f05f845a11add10a04f3fbe29a
bappasahabapi2@gmail.com <bappasahabapi2@gmail.com>
2023-02-16 23:54:56 +0600

    another counter

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ const incrementElement =document.getElementById('increment');
 const decrementElement =document.getElementById('decrement');
 
 
+const counterElement2 =document.getElementById('counter2');
+const incrementElement2 =document.getElementById('increment2');
+const decrementElement2 =document.getElementById('decrement2');
+
+
+
+
 //TODO: SET THE INITIAL STATE
 
 let count =0;
+let count2 =2;
 
 //TODO: ADD EVENT LISTENERS
 
@@ -18,4 +26,13 @@ incrementElement.addEventListener("click",()=>{
 decrementElement.addEventListener("click",()=>{
     count =count-1;
     counterElement.innerText=count;
+})
+
+incrementElement2.addEventListener("click",()=>{
+    count =count+2;
+    counterElement2.innerText=count;
+})
+decrementElement2.addEventListener("click",()=>{
+    count =count-2;
+    counterElement2.innerText=count;
 })
