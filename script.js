7f821b0439e96fde003d9d7aa421e92b1acccc54
bappasahabapi2@gmail.com <bappasahabapi2@gmail.com>
2023-02-17 23:28:40 +0600

    counter using vanila redux

--- script.js
+++ script.js
@@ -1,21 +1,58 @@
-//TODO: SELECT THE DOM ELEMENT
+//TODO: 01.initial state
 
-const counterElement =document.getElementById('counter');
-const incrementElement =document.getElementById('increment');
-const decrementElement =document.getElementById('decrement');
+const initialState ={
+    value:0,
+}
+
+//TODO: 02.create reducer
+
+function counterReducer(state=initialState, action){
+    if (action.type==='increment') {
+        return{
+            ...state,
+            value:state.value+1
+        };
+    }else if(action.type==='decrement'){
+        return{
+            ...state,
+            value:state.value-1
+        };
+    } else{
+        return state;
+    }
+}
+
+//TODO: 03.create store
+
+const store =Redux.createStore(counterReducer);
+
+//TODO: 05.Render in the ui
+
+const render =()=>{
+    const state=store.getState();
+    counterElement.innerText=state.value.toString()
+
+}
+
+store.subscribe(render)
 
 
-//TODO: SET THE INITIAL STATE
+//TODO: 04.button click listener
 
-let count =0;
+// select dom elements
 
-//TODO: ADD EVENT LISTENERS
+const counterElement =document.getElementById('counter')
+const incrementElement =document.getElementById('increment')
+const decrementElement =document.getElementById('decrement')
 
-incrementElement.addEventListener("click",()=>{
-    count =count+1;
-    counterElement.innerText=count;
+incrementElement.addEventListener('click', ()=>{
+    store.dispatch({
+        type:'increment',
+    })
 })
-decrementElement.addEventListener("click",()=>{
-    count =count-1;
-    counterElement.innerText=count;
+decrementElement .addEventListener('click', ()=>{
+    store.dispatch({
+        type:'decrement',
+    })
 })
+
