07dd0eb741b410f050cf9db654eb4356bd40ebdd
bappasahabapi2@gmail.com <bappasahabapi2@gmail.com>
2023-02-24 16:38:42 +0600

    d

--- counter/src/redux/counter/counterReducer.js
+++ counter/src/redux/counter/counterReducer.js
@@ -9,12 +9,12 @@ export const counterReducer = (state = initialState, action) => {
     case INCREMENT:
       return {
         ...state,
-        value: state.value + 1,
+        value: state.value + action.payload,
       };
     case DECREMENT:
       return {
         ...state,
-        value: state.value - 1,
+        value: state.value - action.payload,
       };
 
     default:
