da8350feb0ec0681cde8cdee4fa91fc1e9005879
bappasahabapi2@gmail.com <bappasahabapi2@gmail.com>
2023-02-24 16:25:04 +0600

    f

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,6 +1,6 @@
-import logo from "./logo.svg";
+
 import "./App.css";
-import Counter from "./component/Counter/Counter";
+
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import HooksCounter from "./component/Counter/HooksCounter";
@@ -12,7 +12,7 @@ function App() {
         <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
           Simple Counter Application
         </h1>
-        <Counter id='40' color={'red'} />
+        <HooksCounter id='40' color={'red'} />
         
       </div>
     </Provider>
